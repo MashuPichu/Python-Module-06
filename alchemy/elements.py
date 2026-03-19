082691085df465d12b202942a9b30304ab9fc886
Kimy Lucchini <lucchini.kimy@gmail.com>
2026-03-19 16:51:34 +0100

    1st commit

---
+++ alchemy/elements.py
@@ -0,0 +1,23 @@
+# ************************************************************************* #
+#                                                                           #
+#                                                      :::      ::::::::    #
+#  elements.py                                       :+:      :+:    :+:    #
+#                                                  +:+ +:+         +:+      #
+#  By: klucchin <klucchin@student.42.fr>         +#+  +:+       +#+         #
+#                                              +#+#+#+#+#+   +#+            #
+#  Created: 2026/03/19 16:03:55 by klucchin        #+#    #+#               #
+#  Updated: 2026/03/19 16:14:41 by klucchin        ###   ########.fr        #
+#                                                                           #
+# ************************************************************************* #
+
+def create_fire():
+    return "Fire element created"
+
+def create_water():
+    return "Water element created"
+
+def create_earth():
+    return "Earth element created"
+
+def create_air():
+    return "Air element created"
